898f10cb660e093d00cc8c810f2436fd363891a7
lucas-huang <huanglucas1@gmail.com>
2018-07-06 10:24:10 -0700

    Add files via upload

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(googlesheets)
+
 ui <- function(req) {
   fluidPage(
     div(style = "display: none;",
@@ -10,7 +11,6 @@ ui <- function(req) {
                     req[["REMOTE_ADDR"]]
         )
     ),
-
     titlePanel(
       "Pairwise Comparison App"
     ),
@@ -19,57 +19,57 @@ ui <- function(req) {
       uiOutput("answer1"),
       uiOutput("answer2"),
       uiOutput("cantdecide")
+    ),
+    sidebarPanel(
+      textInput("id", h3("Please enter your ID"), 
+                value = "")
     )
   )
 }
 
 server <- function(input, output, session) {
-  
+
   data <- gs_title("app")
-  question <- as.character(gs_read(data, ws = "surveys", range = "A2:A2", col_names = FALSE))
+  question <- as.character(gs_read(data, ws = "surveys", range = "A3:A3", col_names = FALSE))
   answers <- gs_read(data, ws = "surveys", range = "B2:B100")
   Filter(Negate(is.null), answers)
   answers[sapply(answers, is.null)] <- NULL
   numAnswers <- dim(answers)[1]
-  randomAnswer1 <- answers[sample(1:numAnswers)[1], 1]
-  randomAnswer2 <- answers[sample(1:numAnswers)[1], 1]
-  currentLabel1 <- as.character(randomAnswer1)
-  currentLabel2 <- as.character(randomAnswer2)
-  
-  output$question <- renderText({
-    question
-  })
-  
-  output$answer1 <- renderUI({
-    actionButton("answer1", label = currentLabel1)
-  })
-  
-  output$answer2 <- renderUI({
-    actionButton("answer2", label = currentLabel2)
-  })
-  
-  output$cantdecide <- renderUI({
-    actionButton("cantdecide", label = "Can't Decide")
-  })
+  currentLabel1 <- as.character(answers[sample(1:numAnswers)[1], 1])
+  currentLabel2 <- as.character(answers[sample(1:numAnswers)[1], 1])
   
   #sets the label of the first button
   setLabel1 <- function(){
-    randomAnswer1 <<- answers[sample(1:numAnswers)[1], 1]
-    currentLabel1 <<- as.character(randomAnswer1)
+    currentLabel1 <<- as.character(answers[sample(1:numAnswers)[1], 1])
     return(currentLabel1)
   }
   
   #sets the label of the second button
   setLabel2 <- function(){
-    randomAnswer2 <<- answers[sample(1:numAnswers)[1], 1]
-    currentLabel2 <<- as.character(randomAnswer2)
-    if(randomAnswer1 == randomAnswer2) {
+    currentLabel2 <<- as.character(answers[sample(1:numAnswers)[1], 1])
+    if(currentLabel1 == currentLabel2) {
       setLabel2()
     }
     else {
       return(currentLabel2)
     }
   }
+  
+  output$question <- renderText({
+    question
+  })
+  
+  output$answer1 <- renderUI({
+    actionButton("answer1", label = setLabel1())
+  })
+  
+  output$answer2 <- renderUI({
+    actionButton("answer2", label = setLabel2())
+  })
+  
+  output$cantdecide <- renderUI({
+    actionButton("cantdecide", label = "Can't Decide")
+  })
   
   #gets the label of the first button
   getLabel1 <- function(){
@@ -81,36 +81,35 @@ server <- function(input, output, session) {
     return(currentLabel2)
   }
   
-  # store the counter outside your input/button
-  vars = reactiveValues(counter = 0)
-  
-  # increase the counter
-  observe({
-    if(!is.null(input$click)){
-      input$click
-      isolate({
-        vars$counter <- vars$counter + 1
-      })
+  addRow <- function(x){
+    time <- format(Sys.time(), "%Y-%m-%d %H:%M:%OS3")
+    id <- input$id
+    ip <- (isolate(input$remote_addr))
+    if(x == 1) {
+      gs_add_row(data, ws = "responses", input = data.frame(time, id, ip, question, getLabel1(), getLabel2(), getLabel1()))
+    } else if(x == 2) {
+      gs_add_row(data, ws = "responses", input = data.frame(time, id, ip, question, getLabel1(), getLabel2(), getLabel2()))
+    } else {
+      gs_add_row(data, ws = "responses", input = data.frame(time, id, ip, question, getLabel1(), getLabel2(), "Can't Decide"))
     }
-  })
+  }
   
   observeEvent(input$answer1,{
-    ip <- (isolate(input$remote_addr))
-    gs_add_row(data, ws = "responses", input = c(ip, question, getLabel1(), getLabel2(), getLabel1()))
-    updateActionButton(session, "answer1", label = setLabel1())
-    updateActionButton(session, "answer2", label = setLabel2())
+    addRow(1)
+    setLabel1()
+    setLabel2()
+    updateActionButton(session, "answer1", label = currentLabel1)
+    updateActionButton(session, "answer2", label = currentLabel2)
   })
   
   observeEvent(input$answer2, {
-    ip <- (isolate(input$remote_addr))
-    gs_add_row(data, ws = "responses", input = c(ip, question, getLabel1(), getLabel2(), getLabel2()))
+    addRow(2)
     updateActionButton(session, "answer1", label = setLabel1())
     updateActionButton(session, "answer2", label = setLabel2())
   })
   
   observeEvent(input$cantdecide, {
-    ip <- (isolate(input$remote_addr))
-    gs_add_row(data, ws = "responses", input = c(ip, question, getLabel1(), getLabel2(), "Can't Decide"))
+    addRow(3)
     updateActionButton(session, "answer1", label = setLabel1())
     updateActionButton(session, "answer2", label = setLabel2())
   })
